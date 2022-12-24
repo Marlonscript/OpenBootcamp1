01eb58905bb493ca94f733c6e5a1ed25fbfc38e8
Marlonscript <121361281+Marlonscript@users.noreply.github.com>
2022-12-24 14:34:11 -0800

    Add files via upload
    OpenBootcamp

---
+++ Ejercicio1.java
@@ -0,0 +1,8 @@
+package OpenBootCamp;
+
+public class Ejercicio1 {
+    public static void main(String [] args){
+
+        System.out.println("Tipos de datos: int,long,double,boolean,String");
+    }
+}
